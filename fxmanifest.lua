2099939b5eef5d3630c3240d0509b50d08d6285d
Sweetbounce <sweetbouncedevelopment@gmail.com>
2024-09-26 15:21:18 +0200

    Initial file structure

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Sweetbounce'
+description 'A script to rob mailboxes around the city'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua',
+}
+
+shared_scripts {
+    'shared/*.lua',
+}
+
+dependency 'polyzone'
